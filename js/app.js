d50e979264ff0a7401fa550c327a494ee76c331a
mellaniesoriano <msoriano808@gmail.com>
2017-03-29 20:30:45 -1000

    done

--- js/app.js
+++ js/app.js
@@ -2,3 +2,17 @@
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
 
+var showMenu = document.getElementsByClassName("name");
+
+for(var i = 0; i<showMenu.length; i++){
+  showMenu[i].addEventListener("click", displayMenu);
+}
+
+function displayMenu(){
+var toggleMenu = this.querySelectorAll(".menu")[0];
+if(toggleMenu.style.display === "none"){
+toggleMenu.style.display = "block";
+}else{
+  toggleMenu.style.display = "none";
+  }
+}
